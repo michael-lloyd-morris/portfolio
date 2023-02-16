2aa6cc1ac3b7a742e9cf8e2a6fcd87df993a6f28
Michael Morris <michael.morris@ruralsourcing.com>
2023-02-15 21:53:32 -0500

    Add resume link to home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Page from "@/components/Page"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <Page>
       <h1>Hello World</h1>
       <section>
-        <p>My adventures in computer programming began Christmas 1983 when my parents bought me a <a href="https://en.wikipedia.org/wiki/VIC-20">Commodore VIC-20</a>. I started programming professionally in 2004 and have been contributed to multiple open source projects. I am proficient in the major frontend web technologies and this website is designed as a demonstration of this knowledge. It is built on <a href="https://nextjs.org/">Next.js</a> using <a href="https://www.typescriptlang.org/">Typescript</a> and hosted by <a href="https://vercel.com">Vercel</a>. The source code of this page is in <a href="https://github.com/michael-lloyd-morris/portfolio">this repository</a>.</p>
+        <p>My adventures in computer programming began Christmas 1983 when my parents bought me a <a href="https://en.wikipedia.org/wiki/VIC-20">Commodore VIC-20</a>. I started programming professionally in 2004 and have been contributed to multiple open source projects. I am proficient in the major frontend web technologies and this website is designed as a demonstration of this knowledge. It is built on <a href="https://nextjs.org/">Next.js</a> using <a href="https://www.typescriptlang.org/">Typescript</a> and hosted by <a href="https://vercel.com">Vercel</a>. The source code of this page is in <a href="https://github.com/michael-lloyd-morris/portfolio">this repository</a>. My resume is <Link href="/resume.pdf">here</Link>.</p>
         <p>I am currently looking for work on a remote basis. I cannot relocate because I am caring for my elderly parents.</p>
       </section>
       <section>
