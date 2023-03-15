97649bb95022cacdb2bcb02ca7959cfbc7af1a49
Michael Morris <michael.morris@ruralsourcing.com>
2023-03-15 15:13:12 -0400

    CORS doesn't like http requests from https origins

--- components/DrupalPagesQuery.ts
+++ components/DrupalPagesQuery.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const DrupalPagesQuery = {
   key: ['drupalQuery'],
-  query: () => fetch(`http://drupal.mmorris-sandbox.net/pages?_format=json`)
+  query: () => fetch(`//drupal.mmorris-sandbox.net/pages?_format=json`)
   .then(response => response.json())
 };
 
